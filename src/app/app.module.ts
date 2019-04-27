d5e5b60facdbec7b42b8c637502dcd100df57ce1
Miguel <mikeramirez-14-31@hotmail.com>
2019-04-27 00:09:43 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PersonaService } from './service/persona.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [
+    PersonaService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PersonaService } from '../service/persona.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  data: any;
+
+  constructor(
+    public personaService: PersonaService
+  ) {
+
+    this.data =  {
+      nombre: '',
+      apellido: '',
+      edad: null
+    }
+  }
+
+  enviarForm(data) {
+    console.log(data)
+
+    let datosAEnviar: any = {
+      primerNombre: data.nombre,
+      segundoNombre: data.apellido,
+      edad: data.edad
+    };
+
+    console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.create(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+  }
+
+  actualizarForm(data) {
+    console.log(data)
+  
+    let datosAEnviar: any = {
+      primerNombre: data.nombre,
+      segundoNombre: data.apellido,
+      edad: data.edad
+    };
+
+    console.log('Datos a enviar:' + JSON.stringify(datosAEnviar));
+
+    this.personaService.update(datosAEnviar).subscribe(result => {
+      console.log('Datos from server:' + JSON.stringify(result));
+    });
+
+  }
 
 }
