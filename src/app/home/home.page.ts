5f7c8008f12ca6ddf63547f7583be7c69af81e70
Franco <kevinfranco199@gmail.com>
2019-06-05 18:26:39 -0600

    modified:   src/app/home/home.page.html 	modified:   src/app/home/home.page.ts 	modified:   src/app/service/notification.service.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { PersonaService } from '../service/persona.service';
+import { NotificationService } from '../service/notification.service';
+import { BehaviorSubject } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
+  private mensaje: String = null;
+  public mySubject: BehaviorSubject<any>;
+  public dataTable = [];
   data: any;
 
   constructor(
-    public personaService: PersonaService
+    public personaService: PersonaService,
+    public notificationService: NotificationService
   ) {
+
+    this.mySubject = new BehaviorSubject(null);
 
     this.data =  {
       email: '',
@@ -20,7 +29,7 @@ export class HomePage {
     };
   }
 
-  enviarForm(data) {
+  public enviarForm(data) {
     console.log(data)
 
     let datosAEnviar: any = {
@@ -35,7 +44,7 @@ export class HomePage {
     });
   }
 
-  actualizarForm(data) {
+  public actualizarForm(data) {
     console.log(data);
       let datosAEnviar: any = {
       email: data.email,
@@ -49,7 +58,7 @@ export class HomePage {
     });
 
   }
-  mostrarDatos(data) {
+  public mostrarDatos(data) {
     console.log(data);
 
     const datosAEnviar: any = {
@@ -61,5 +70,69 @@ export class HomePage {
     this.personaService.getAll(datosAEnviar).subscribe(result => {
       console.log('Datos from server:' + JSON.stringify(result));
     });
+  }
+
+  private handleMessageReceived(message: any): void {
+    console.log('Menseje recibido' + JSON.stringify(message));
+   
+  }
+
+  public doNotificationSubscription(): void{
+
+    try{
+      this.notificationService.getPersonaNotification().subscribe((result) =>{
+        this.handleMessageReceived(result);
+  
+      });
+    } catch(e){
+      console.log(e);
+    }
+  }
+
+  private initData(data) {
+
+    this.data = ({
+      email: data.email,
+      usuario: data.usuario
+    });
+
+  }
+
+  public ProcesarMySubject(result: any): void{
+    console.log('hacer algo con: ' + JSON.stringify(result));
+    this.mensaje = this.mensaje + '' + JSON.stringify(result);
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    console.log('on after view');
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    console.log('on destroy');
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    console.log('on init'),
+
+    this.doNotificationSubscription();
+
+    this.initData(this.data);
+
+    this.mySubject.subscribe((result) => {
+      this.ProcesarMySubject(result)
+    });
+
+    this.personaService
+      .personaList(null)
+      .subscribe((result) => {
+        console.log('Resultado:' + JSON.stringify(result));
+      });
   }
 }
